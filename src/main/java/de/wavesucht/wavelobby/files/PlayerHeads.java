f96914402df321d42a078b2cc059ef1cd38e24e8
PsychoGamer_95 <69249795+Fukanoherde@users.noreply.github.com>
2021-05-01 17:24:09 +0200

    Create PlayerHeads.java

---
+++ src/main/java/de/wavesucht/wavelobby/files/PlayerHeads.java
@@ -0,0 +1,265 @@
+package de.wavesucht.wavelobby.files;
+
+import de.wavesucht.wavelobby.WaveLobby;
+
+public class PlayerHeads {
+        private static FileWriter fileWriter = new FileWriter(WaveLobby.getPlugin().getDataFolder().getPath(), "heads.yml");
+
+        public static void loadFile() {
+
+            setValue("Plugin.Entwickler", "Fredd_HD and PsychoGamer_95");
+            setValue("No.Heads.Buy", " ");
+            setValue("heads.buy.lore", "Gekauft");
+            setValue("heads.no.buy.lore", "Nicht gekauft");
+
+            // Heads Team \\
+
+            setValue("Head.Team.0", "RonteckX");
+            setValue("Head.Team.1", "Fredd_HD");
+            setValue("Head.Team.2", "LionGaming_95");
+            setValue("Head.Team.3", "Caleric");
+            setValue("Head.Team.free", " ");
+
+            // Head Team DisplayName \\
+
+            setValue("Head.Display.Team.0", "&6➤ RonteckX");
+            setValue("Head.Display.Team.1", "&6➤ Fredd_HD");
+            setValue("Head.Display.Team.2", "&6➤ LionGaming_95");
+            setValue("Head.Display.Team.3", "&6➤ Caleric");
+
+            // Heads Name \\
+
+            setValue("Head.Name.0", "MrCodingMen");
+            setValue("Head.Name.1", "MrSnowDK");
+            setValue("Head.Name.2", "Tobi_Bln");
+            setValue("Head.Name.3", "Noxed10");
+            setValue("Head.Name.4", "melvinhaze");
+            setValue("Head.Name.5", "ABigDwarf");
+            setValue("Head.Name.6", "Magearna");
+            setValue("Head.Name.7", "ilikminecraft");
+            setValue("Head.Name.8", "FluffyDiamond92");
+            setValue("Head.Name.9", "Unknown_Is_Gay");
+            setValue("Head.Name.10", "ZeeFear");
+            setValue("Head.Name.11", "Metroidling");
+            setValue("Head.Name.12", "0qt");
+            setValue("Head.Name.13", "WishingTie09120");
+            setValue("Head.Name.14", "Squattamelon");
+            setValue("Head.Name.15", "aardbeiijsje");
+            setValue("Head.Name.16", "ghosterystore");
+            setValue("Head.Name.17", "julagamer2007");
+            setValue("Head.Name.18", "Entity_303");
+            setValue("Head.Name.19", "BlueBear121");
+            setValue("Head.Name.20", "Umbreon_");
+            setValue("Head.Name.21", "Byere");
+            setValue("Head.Name.22", "404");
+            setValue("Head.Name.23", "DaBusinessDream");
+            setValue("Head.Name.24", "SystemAPI");
+            setValue("Head.Name.25", "Mr_FreddyFazbear");
+            setValue("Head.Name.26", "PrestonPlayz");
+            setValue("Head.Name.27", "Joskee_");
+            setValue("Head.Name.28", "MHF_YouTube");
+            setValue("Head.Name.29", "MonkeyDeep123");
+            setValue("Head.Name.30", "Thanatos_");
+            setValue("Head.Name.31", "dfgsteam");
+            setValue("Head.Name.32", "Axel1103");
+            setValue("Head.Name.33", "raichuthink");
+            setValue("Head.Name.34", "Ruan");
+            setValue("Head.Name.35", "_Kaine_");
+
+
+            // Head DisplayName \\
+
+            setValue("Head.Display.Name.0", "&6➤ MrCodingMen");
+            setValue("Head.Display.Name.1", "&6➤ MrSnowDK");
+            setValue("Head.Display.Name.2", "&6➤ Tobi_Bln");
+            setValue("Head.Display.Name.3", "&6➤ Noxed10");
+            setValue("Head.Display.Name.4", "&6➤ melvinhaze");
+            setValue("Head.Display.Name.5", "&6➤ ABigDwarf");
+            setValue("Head.Display.Name.6", "&6➤ Magearna");
+            setValue("Head.Display.Name.7", "&6➤ ilikminecraft");
+            setValue("Head.Display.Name.8", "&6➤ FluffyDiamond92");
+            setValue("Head.Display.Name.9", "&6➤ Unknown_Is_Gay");
+            setValue("Head.Display.Name.10", "&6➤ ZeeFear");
+            setValue("Head.Display.Name.11", "&6➤ Metroidling");
+            setValue("Head.Display.Name.12", "&6➤ 0qt");
+            setValue("Head.Display.Name.13", "&6➤ WishingTie09120");
+            setValue("Head.Display.Name.14", "&6➤ Squattamelon");
+            setValue("Head.Display.Name.15", "&6➤ aardbeiijsje");
+            setValue("Head.Display.Name.16", "&6➤ ghosterystore");
+            setValue("Head.Display.Name.17", "&6➤ julagamer2007");
+            setValue("Head.Display.Name.18", "&6➤ Entity_303");
+            setValue("Head.Display.Name.19", "&6➤ BlueBear121");
+            setValue("Head.Display.Name.20", "&6➤ Umbreon_");
+            setValue("Head.Display.Name.21", "&6➤ Byere");
+            setValue("Head.Display.Name.22", "&6➤ 404");
+            setValue("Head.Display.Name.23", "&6➤ DaBusinessDream");
+            setValue("Head.Display.Name.24", "&6➤ SystemAPI");
+            setValue("Head.Display.Name.25", "&6➤ Mr_FreddyFazbear");
+            setValue("Head.Display.Name.26", "&6➤ PrestonPlayz");
+            setValue("Head.Display.Name.27", "&6➤ Joskee_");
+            setValue("Head.Display.Name.28", "&6➤ MHF_YouTube");
+            setValue("Head.Display.Name.29", "&6➤ MonkeyDeep123");
+            setValue("Head.Display.Name.30", "&6➤ Thanatos_");
+            setValue("Head.Display.Name.31", "&6➤ dfgsteam");
+            setValue("Head.Display.Name.32", "&6➤ Axel1103");
+            setValue("Head.Display.Name.33", "&6➤ raichuthink");
+            setValue("Head.Display.Name.34", "&6➤ Ruan");
+            setValue("Head.Display.Name.35", "&6➤ _Kaine_");
+
+            // Head Youtuber \\
+
+            setValue("Head.Name.YT.0", "UniSpielt");
+            setValue("Head.Name.YT.1", "ungespielt");
+            setValue("Head.Name.YT.2", "DezztroyzClient");
+            setValue("Head.Name.YT.3", "Rypex");
+            setValue("Head.Name.YT.4", "BastiGHG");
+            setValue("Head.Name.YT.5", "Alphastein");
+            setValue("Head.Name.YT.6", "Paluten");
+            setValue("Head.Name.YT.7", "GommeHD");
+            setValue("Head.Name.YT.8", "SparkofPhoenix ");
+
+            // Head Youtuber Display \\
+
+            setValue("Head.Display.Name.YT.0", "&5UniSpielt");
+            setValue("Head.Display.Name.YT.1", "&5ungespielt");
+            setValue("Head.Display.Name.YT.2", "&5DezztroyzClient");
+            setValue("Head.Display.Name.YT.3", "&5Rypex");
+            setValue("Head.Display.Name.YT.4", "&5BastiGHG");
+            setValue("Head.Display.Name.YT.5", "&5Alphastein");
+            setValue("Head.Display.Name.YT.6", "&5Paluten");
+            setValue("Head.Display.Name.YT.7", "&5GommeHD");
+            setValue("Head.Display.Name.YT.8", "&5SparkofPhoenix");
+
+            // Head Block DisplayName \\
+
+            setValue("Head.Display.Name.Block.0", "&6➤ ");
+            setValue("Head.Display.Name.Block.1", "&6➤ ");
+            setValue("Head.Display.Name.Block.2", "&6➤ ");
+            setValue("Head.Display.Name.Block.3", "&6➤ ");
+            setValue("Head.Display.Name.Block.4", "&6➤ ");
+            setValue("Head.Display.Name.Block.5", "&6➤ ");
+            setValue("Head.Display.Name.Block.6", "&6➤ ");
+            setValue("Head.Display.Name.Block.7", "&6➤ ");
+            setValue("Head.Display.Name.Block.8", "&6➤ ");
+            setValue("Head.Display.Name.Block.9", "&6➤ ");
+            setValue("Head.Display.Name.Block.10", "&6➤ ");
+            setValue("Head.Display.Name.Block.11", "&6➤ ");
+            setValue("Head.Display.Name.Block.12", "&6➤ ");
+            setValue("Head.Display.Name.Block.13", "&6➤ ");
+            setValue("Head.Display.Name.Block.14", "&6➤ ");
+            setValue("Head.Display.Name.Block.15", "&6➤ ");
+            setValue("Head.Display.Name.Block.16", "&6➤ ");
+            setValue("Head.Display.Name.Block.17", "&6➤ ");
+            setValue("Head.Display.Name.Block.18", "&6➤ ");
+            setValue("Head.Display.Name.Block.19", "&6➤ ");
+            setValue("Head.Display.Name.Block.20", "&6➤ ");
+            setValue("Head.Display.Name.Block.21", "&6➤ ");
+            setValue("Head.Display.Name.Block.22", "&6➤ ");
+            setValue("Head.Display.Name.Block.23", "&6➤ ");
+            setValue("Head.Display.Name.Block.24", "&6➤ ");
+            setValue("Head.Display.Name.Block.25", "&6➤ ");
+            setValue("Head.Display.Name.Block.26", "&6➤ ");
+            setValue("Head.Display.Name.Block.27", "&6➤ ");
+            setValue("Head.Display.Name.Block.28", "&6➤ ");
+            setValue("Head.Display.Name.Block.29", "&6➤ ");
+            setValue("Head.Display.Name.Block.30", "&6➤ ");
+
+            // Head Perms YT \\
+
+            setValue("Head.YT.Perm.0", "lobby.heads.0");
+            setValue("Head.YT.Perm.1", "lobby.heads.1");
+            setValue("Head.YT.Perm.2", "lobby.heads.2");
+            setValue("Head.YT.Perm.3", "lobby.heads.3");
+            setValue("Head.YT.Perm.4", "lobby.heads.4");
+            setValue("Head.YT.Perm.5", "lobby.heads.5");
+            setValue("Head.YT.Perm.6", "lobby.heads.6");
+            setValue("Head.YT.Perm.7", "lobby.heads.7");
+            setValue("Head.YT.Perm.8", "lobby.heads.8");
+            setValue("Head.YT.Perm.9", "lobby.heads.9");
+
+            // Head Perms \\
+
+            setValue("Head.Perm.0", "lobby.heads.0");
+            setValue("Head.Perm.1", "lobby.heads.1");
+            setValue("Head.Perm.2", "lobby.heads.2");
+            setValue("Head.Perm.3", "lobby.heads.3");
+            setValue("Head.Perm.4", "lobby.heads.4");
+            setValue("Head.Perm.5", "lobby.heads.5");
+            setValue("Head.Perm.6", "lobby.heads.6");
+            setValue("Head.Perm.7", "lobby.heads.7");
+            setValue("Head.Perm.8", "lobby.heads.8");
+            setValue("Head.Perm.9", "lobby.heads.9");
+            setValue("Head.Perm.10", "lobby.heads.10");
+            setValue("Head.Perm.11", "lobby.heads.11");
+            setValue("Head.Perm.12", "lobby.heads.12");
+            setValue("Head.Perm.13", "lobby.heads.13");
+            setValue("Head.Perm.14", "lobby.heads.14");
+            setValue("Head.Perm.15", "lobby.heads.15");
+            setValue("Head.Perm.16", "lobby.heads.16");
+            setValue("Head.Perm.17", "lobby.heads.17");
+            setValue("Head.Perm.18", "lobby.heads.18");
+            setValue("Head.Perm.19", "lobby.heads.19");
+            setValue("Head.Perm.20", "lobby.heads.20");
+            setValue("Head.Perm.21", "lobby.heads.21");
+            setValue("Head.Perm.22", "lobby.heads.22");
+            setValue("Head.Perm.23", "lobby.heads.23");
+            setValue("Head.Perm.24", "lobby.heads.24");
+            setValue("Head.Perm.25", "lobby.heads.25");
+            setValue("Head.Perm.26", "lobby.heads.26");
+            setValue("Head.Perm.27", "lobby.heads.27");
+            setValue("Head.Perm.28", "lobby.heads.28");
+            setValue("Head.Perm.29", "lobby.heads.29");
+            setValue("Head.Perm.30", "lobby.heads.30");
+            setValue("Head.Perm.31", "lobby.heads.30");
+            setValue("Head.Perm.32", "lobby.heads.30");
+            setValue("Head.Perm.33", "lobby.heads.30");
+            setValue("Head.Perm.34", "lobby.heads.30");
+            setValue("Head.Perm.35", "lobby.heads.30");
+
+            // Head Block Perms \\
+
+            setValue("Head.Block.Perm.0", "lobby.heads.block.0");
+            setValue("Head.Block.Perm.1", "lobby.heads.block.1");
+            setValue("Head.Block.Perm.2", "lobby.heads.block.2");
+            setValue("Head.Block.Perm.3", "lobby.heads.block.3");
+            setValue("Head.Block.Perm.4", "lobby.heads.block.4");
+            setValue("Head.Block.Perm.5", "lobby.heads.block.5");
+            setValue("Head.Block.Perm.6", "lobby.heads.block.6");
+            setValue("Head.Block.Perm.7", "lobby.heads.block.7");
+            setValue("Head.Block.Perm.8", "lobby.heads.block.8");
+            setValue("Head.Block.Perm.9", "lobby.heads.block.9");
+            setValue("Head.Block.Perm.10", "lobby.heads.block.10");
+            setValue("Head.Block.Perm.11", "lobby.heads.block.11");
+            setValue("Head.Block.Perm.12", "lobby.heads.block.12");
+            setValue("Head.Block.Perm.13", "lobby.heads.block.13");
+            setValue("Head.Block.Perm.14", "lobby.heads.block.14");
+            setValue("Head.Block.Perm.15", "lobby.heads.block.15");
+            setValue("Head.Block.Perm.16", "lobby.heads.block.16");
+            setValue("Head.Block.Perm.17", "lobby.heads.block.17");
+            setValue("Head.Block.Perm.18", "lobby.heads.block.18");
+            setValue("Head.Block.Perm.19", "lobby.heads.block.19");
+            setValue("Head.Block.Perm.20", "lobby.heads.block.20");
+            setValue("Head.Block.Perm.21", "lobby.heads.block.21");
+            setValue("Head.Block.Perm.22", "lobby.heads.block.22");
+            setValue("Head.Block.Perm.23", "lobby.heads.block.23");
+            setValue("Head.Block.Perm.24", "lobby.heads.block.24");
+            setValue("Head.Block.Perm.25", "lobby.heads.block.25");
+            setValue("Head.Block.Perm.26", "lobby.heads.block.26");
+            setValue("Head.Block.Perm.27", "lobby.heads.block.27");
+            setValue("Head.Block.Perm.28", "lobby.heads.block.28");
+            setValue("Head.Block.Perm.29", "lobby.heads.block.29");
+            setValue("Head.Block.Perm.30", "lobby.heads.block.30");
+
+        }
+
+        private static void setValue(String valuePath, String value) {
+            if (!fileWriter.valueExist(valuePath)) {
+                fileWriter.setValue(valuePath, value);
+                fileWriter.save();
+            }
+        }
+
+        public static Object getValue(String valuePath) {
+            return fileWriter.getObject(valuePath);
+    }
+}
